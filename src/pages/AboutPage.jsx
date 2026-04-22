330b12598c83b616e9eae102b7939160bb528f98
umer jusani <umer.jusani@gesolucions.com>
2026-04-22 20:34:33 +0500

    bu fixesasdsad

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,14 +1,17 @@
 import SampleInfo from '../components/SampleInfo.jsx'
 import { getSampleMessage } from '../utils/index.js'
+import ContactPage from './ContactPage.jsx'
 
 function AboutPage() {
   return (
     <>
-      <h2>About Pagess</h2>
+      <h2>About Pagsssss</h2>
       <SampleInfo
         title="About Shared Component"
         text="Yeh reusable component About aur Contact dono pages me use ho raha hai."
       />
+
+      <ContactPage />
       <p>{getSampleMessage('About Page')}</p>
     </>
   )
